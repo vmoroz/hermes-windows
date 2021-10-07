decc44e299831e28e856fe5bef0f0c1d35608a2b
Vladimir Morozov <vmorozov@microsoft.com>
2021-10-07 13:22:13 -0700

    Format code

--- API/hermes/js_native_ext_api.h
+++ API/hermes/js_native_ext_api.h
@@ -16,19 +16,22 @@
 
 EXTERN_C_START
 
-//TODO: Revise relevance. Likely obsoleted by napi_ext_env_settings::flags
-typedef enum
-{
-  napi_ext_env_attribute_none =                       0x00000000,
-  napi_ext_env_attribute_enable_gc_api =              0x00000001,
-  napi_ext_env_attribute_ignore_unhandled_promises =  0x00000002,
+// TODO: Revise relevance. Likely obsoleted by napi_ext_env_settings::flags
+typedef enum {
+  napi_ext_env_attribute_none = 0x00000000,
+  napi_ext_env_attribute_enable_gc_api = 0x00000001,
+  napi_ext_env_attribute_ignore_unhandled_promises = 0x00000002,
 } napi_ext_env_attributes;
 
 typedef struct napi_ext_env_scope__ *napi_ext_env_scope;
 typedef struct napi_ext_ref__ *napi_ext_ref;
 
 // A callback to return buffer synchronously
-typedef void (*napi_ext_buffer_callback)(napi_env env, uint8_t const *buffer, size_t buffer_length, void *buffer_hint);
+typedef void (*napi_ext_buffer_callback)(
+    napi_env env,
+    uint8_t const *buffer,
+    size_t buffer_length,
+    void *buffer_hint);
 
 // A callback to run task
 typedef void (*napi_ext_task_callback)(napi_env env, void *task_data);
@@ -42,8 +45,7 @@ typedef void (*napi_ext_schedule_task_callback)(
     napi_finalize finalize_cb,
     void *finalize_hint);
 
-typedef struct _napi_ext_env_settings
-{
+typedef struct _napi_ext_env_settings {
   // Size of this struct to allow extending it in future.
   size_t this_size;
 
@@ -67,50 +69,58 @@ typedef struct _napi_ext_env_settings
   // Additional data for the finalize callback.
   void *finalize_data_hint;
 
-//NOTE: Keep in sync with v8runtime::V8RuntimeArgs::flags
-// Padded to allow adding boolean flags without breaking the ABI
-  union
-  {
-    struct
-    {
-      bool track_gc_object_stats:1;
-      bool enable_jit_tracing:1;
-      bool enable_message_tracing:1;
-      bool enable_gc_tracing:1;
-      bool enable_inspector:1;
-      bool wait_for_debugger:1;
-      bool enable_gc_api:1;
-      bool ignore_unhandled_promises:1;
-      bool enable_system_instrumentation:1;
+  // NOTE: Keep in sync with v8runtime::V8RuntimeArgs::flags
+  // Padded to allow adding boolean flags without breaking the ABI
+  union {
+    struct {
+      bool track_gc_object_stats : 1;
+      bool enable_jit_tracing : 1;
+      bool enable_message_tracing : 1;
+      bool enable_gc_tracing : 1;
+      bool enable_inspector : 1;
+      bool wait_for_debugger : 1;
+      bool enable_gc_api : 1;
+      bool ignore_unhandled_promises : 1;
+      bool enable_system_instrumentation : 1;
 
       // Experimental flags (for memory-constrained optimization testing)
-      bool sparkplug:1; // https://v8.dev/blog/sparkplug
-      bool predictable:1; // take a big CPU hit to reduce the number of threads
-      bool optimize_for_size:1; // enables optimizations which favor memory size over execution speed
-      bool always_compact:1; // perform compaction on every full GC
-      bool jitless:1; // disable JIT entirely
+      bool sparkplug : 1; // https://v8.dev/blog/sparkplug
+      bool
+          predictable : 1; // take a big CPU hit to reduce the number of threads
+      bool optimize_for_size : 1; // enables optimizations which favor memory
+                                  // size over execution speed
+      bool always_compact : 1; // perform compaction on every full GC
+      bool jitless : 1; // disable JIT entirely
     } flags;
-    uint32_t _flagspad {0};
+    uint32_t _flagspad{0};
   };
 
 } napi_ext_env_settings;
 
 // Creates a new napi_env with ref count 1.
-NAPI_EXTERN napi_status __cdecl napi_ext_create_env(napi_ext_env_settings *settings, napi_env *env);
+NAPI_EXTERN napi_status __cdecl napi_ext_create_env(
+    napi_ext_env_settings *settings,
+    napi_env *env);
 
 // Increments the napi_env ref count by 1.
 NAPI_EXTERN napi_status __cdecl napi_ext_env_ref(napi_env env);
 
-// Decrements the napi_env ref count by 1. If the ref count becomes 0, then the napi_env is deleted.
+// Decrements the napi_env ref count by 1. If the ref count becomes 0, then the
+// napi_env is deleted.
 NAPI_EXTERN napi_status __cdecl napi_ext_env_unref(napi_env env);
 
 // Opens the napi_env in the current thread.
 // Calling N-API functions without the opened scope may cause a failure.
 // The scope must be closed by the napi_ext_close_env_scope call.
-NAPI_EXTERN napi_status __cdecl napi_ext_open_env_scope(napi_env env, napi_ext_env_scope *result);
+NAPI_EXTERN napi_status __cdecl napi_ext_open_env_scope(
+    napi_env env,
+    napi_ext_env_scope *result);
 
-// Closes the napi_env in the current thread. It must match to the napi_ext_open_env_scope call.
-NAPI_EXTERN napi_status __cdecl napi_ext_close_env_scope(napi_env env, napi_ext_env_scope scope);
+// Closes the napi_env in the current thread. It must match to the
+// napi_ext_open_env_scope call.
+NAPI_EXTERN napi_status __cdecl napi_ext_close_env_scope(
+    napi_env env,
+    napi_ext_env_scope scope);
 
 // Runs script with the provided source_url origin.
 NAPI_EXTERN napi_status __cdecl napi_ext_run_script(
@@ -141,10 +151,15 @@ NAPI_EXTERN napi_status __cdecl napi_ext_serialize_script(
 NAPI_EXTERN napi_status __cdecl napi_ext_collect_garbage(napi_env env);
 
 // Checks if the environment has an unhandled promise rejection.
-NAPI_EXTERN napi_status __cdecl napi_ext_has_unhandled_promise_rejection(napi_env env, bool *result);
+NAPI_EXTERN napi_status __cdecl napi_ext_has_unhandled_promise_rejection(
+    napi_env env,
+    bool *result);
 
 // Gets and clears the last unhandled promise rejection.
-NAPI_EXTERN napi_status __cdecl napi_get_and_clear_last_unhandled_promise_rejection(napi_env env, napi_value *result);
+NAPI_EXTERN
+    napi_status __cdecl napi_get_and_clear_last_unhandled_promise_rejection(
+        napi_env env,
+        napi_value *result);
 
 // Use to enable fast string equality check by comparing napi_refs as addresses.
 // The caller is responsible for calling napi_reference_unref on the result
@@ -157,14 +172,20 @@ NAPI_EXTERN napi_status __cdecl napi_ext_get_unique_string_utf8_ref(
 
 // Gets an unique string reference.
 NAPI_EXTERN
-napi_status __cdecl napi_ext_get_unique_string_ref(napi_env env, napi_value str_value, napi_ext_ref *result);
+napi_status __cdecl napi_ext_get_unique_string_ref(
+    napi_env env,
+    napi_value str_value,
+    napi_ext_ref *result);
 
 // Methods to control object lifespan.
 // The NAPI's napi_ref can be used only for objects.
 // The napi_ext_ref can be used for any value type.
 
 // Creates new napi_ext_ref with ref counter set to 1.
-NAPI_EXTERN napi_status __cdecl napi_ext_create_reference(napi_env env, napi_value value, napi_ext_ref *result);
+NAPI_EXTERN napi_status __cdecl napi_ext_create_reference(
+    napi_env env,
+    napi_value value,
+    napi_ext_ref *result);
 
 // Creates new napi_ext_ref and associates native data with the reference.
 // The ref counter is set to 1.
@@ -178,19 +199,30 @@ NAPI_EXTERN napi_status __cdecl napi_ext_create_reference_with_data(
 
 // Creates new napi_ext_ref with ref counter set to 1.
 // The napi_ext_ref wraps up a weak reference to the value.
-// Even if the napi_ext_ref ref counter is more than 0, the internal weak reference can exptire and the
-// napi_ext_get_reference_value for this napi_ext_ref may return nullptr.
-NAPI_EXTERN napi_status __cdecl napi_ext_create_weak_reference(napi_env env, napi_value value, napi_ext_ref *result);
+// Even if the napi_ext_ref ref counter is more than 0, the internal weak
+// reference can exptire and the napi_ext_get_reference_value for this
+// napi_ext_ref may return nullptr.
+NAPI_EXTERN napi_status __cdecl napi_ext_create_weak_reference(
+    napi_env env,
+    napi_value value,
+    napi_ext_ref *result);
 
 // Increments the reference count.
-NAPI_EXTERN napi_status __cdecl napi_ext_reference_ref(napi_env env, napi_ext_ref ref);
+NAPI_EXTERN napi_status __cdecl napi_ext_reference_ref(
+    napi_env env,
+    napi_ext_ref ref);
 
 // Decrements the reference count.
 // The provided ref must not be used after this call because it could be deleted
 // if the internal ref counter became zero.
-NAPI_EXTERN napi_status __cdecl napi_ext_reference_unref(napi_env env, napi_ext_ref ref);
+NAPI_EXTERN napi_status __cdecl napi_ext_reference_unref(
+    napi_env env,
+    napi_ext_ref ref);
 
 // Gets the referenced value.
-NAPI_EXTERN napi_status __cdecl napi_ext_get_reference_value(napi_env env, napi_ext_ref ref, napi_value *result);
+NAPI_EXTERN napi_status __cdecl napi_ext_get_reference_value(
+    napi_env env,
+    napi_ext_ref ref,
+    napi_value *result);
 
 EXTERN_C_END
