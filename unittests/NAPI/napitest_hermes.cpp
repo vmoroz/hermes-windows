89981660162ab53ecabde379966bf83694c6cd74
Vladimir Morozov <vmorozov@microsoft.com>
2022-01-09 18:18:18 -0800

    Add unit test files

---
+++ unittests/NAPI/napitest_hermes.cpp
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+#include <gtest/gtest.h>
+#include "napitest.h"
+
+namespace napitest {
+
+std::vector<NapiEnvFactory> NapiEnvFactories() {
+  return {[]() {
+    napi_env env{};
+    napi_ext_env_settings settings{};
+    settings.this_size = sizeof(napi_ext_env_settings);
+    settings.flags.enable_gc_api = true;
+    napi_ext_create_env(&settings, &env);
+    return env;
+  }};
+}
+
+} // namespace napitest
