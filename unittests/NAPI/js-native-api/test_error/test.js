7fac27a692dfff0b45e36490c53b3768fc17c913
Vladimir Morozov <vmorozov@microsoft.com>
2022-01-25 07:04:28 -0800

    Synchronize scripts with V8-JSI

--- unittests/NAPI/js-native-api/test_error/test.js
+++ unittests/NAPI/js-native-api/test_error/test.js
@@ -11,30 +11,7 @@ const theReferenceError = new ReferenceError('Some reference error');
 const theURIError = new URIError('Some URI error');
 const theEvalError = new EvalError('Some eval error');
 
-// class MyError extends Error { }
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var MyError = /** @class */ (function (_super) {
-    __extends(MyError, _super);
-    function MyError() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    return MyError;
-}(Error));
-
+class MyError extends Error { }
 const myError = new MyError('Some MyError');
 
 // Test that native error object is correctly classed
